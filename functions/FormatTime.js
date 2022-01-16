2723a9b3d10ac9ddbf92737077c768862eca6fd1
Dylan <dylank09@gmail.com>
2022-01-16 14:30:18 +0000

    Changing format of time to iso

--- functions/FormatTime.js
+++ functions/FormatTime.js
@@ -4,12 +4,21 @@ export default function FormatTime(timestamp) {
   }
   var t = new Date(1970, 0, 1); // Epoch
   t.setSeconds(timestamp);
-  let today = new Date().toLocaleString() + "";
-  timestamp = t.toLocaleString() + "";
-  if (today.slice(0, 10) == timestamp.slice(0, 10)) {
-    timestamp = timestamp.slice(12, 17);
+  var today = new Date().toISOString() + "";
+  timestamp = t.toISOString() + "";
+  let tpos = timestamp.indexOf("T");
+
+  console.log("today: ", today);
+  console.log("ts: ", timestamp);
+  console.log("tpos: ", tpos);
+
+  if (today.slice(0, tpos) !== timestamp.slice(0, tpos)) {
+    timestamp = timestamp.slice(5, tpos);
+    let day = timestamp.slice(3, 5);
+    let month = timestamp.slice(0, 2);
+    timestamp = day + "/" + month;
   } else {
-    timestamp = timestamp.slice(0, 5);
+    timestamp = timestamp.slice(tpos + 1, 16);
   }
   return timestamp;
 }
---
+++ functions/GetRemaining.js
@@ -0,0 +1,15 @@
+export default function GetRemaining(timestamp) {
+  // if (!timestamp) {
+  //   return "";
+  // }
+  // var t = new Date(1970, 0, 1); // Epoch
+  // t.setSeconds(timestamp);
+  // let today = new Date().toLocaleString() + "";
+  // timestamp = t.toLocaleString() + "";
+  // if (today.slice(0, 10) == timestamp.slice(0, 10)) {
+  //   timestamp = timestamp.slice(12, 17);
+  // } else {
+  //   timestamp = timestamp.slice(0, 5);
+  // }
+  // return timestamp;
+}
