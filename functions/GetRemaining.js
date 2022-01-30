d013add8fb61719a7b0e0c8b7edf182d54816450
Dylan <dylank09@gmail.com>
2022-01-30 15:37:34 +0000

    fix remaining time bug

--- functions/GetRemaining.js
+++ functions/GetRemaining.js
@@ -7,23 +7,12 @@ export default function GetRemaining(timestamp) {
   var daysDifference = secondsDifference / (3600 * 24);
   var numDays = Math.floor(daysDifference);
 
-  var t = new Date(1970, 0, 1); // Epoch
-  t.setSeconds(timestamp.seconds);
-  var todayISO = new Date().toISOString() + "";
-  var deadlineISO = t.toISOString() + "";
-  let tpos = deadlineISO.indexOf("T");
-
-  var numHours =
-    parseInt(deadlineISO.slice(tpos + 1, tpos + 3)) -
-    parseInt(todayISO.slice(tpos + 1, tpos + 3));
-
   var numMinutes = Math.floor(secondsDifference / 60);
 
   if (numDays > 0) {
-    var dayString, hourString;
-    dayString = numDays === 1 ? " day, " : " days, ";
-    hourString = numHours === 1 ? " hour" : " hours";
-    return numDays + dayString + numHours + hourString;
+    var dayString;
+    dayString = numDays === 1 ? " day " : " days ";
+    return numDays + dayString;
   } else if (numMinutes > 59) {
     return Math.floor(numMinutes / 60) + " hours";
   } else if (numMinutes > 0) {
